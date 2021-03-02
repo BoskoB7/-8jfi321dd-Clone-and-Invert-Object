f0834f937e5915483b76bdeb21334088d12a5936
BoskoB7 <73939828+BoskoB7@users.noreply.github.com>
2021-03-02 04:11:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var name = "sda"; //string
+var age = 42; //integer
+var temperature = 37.2; //float
+var cities = ['Belgrade', 'Nis']; //array
+var goggedIn = true //boolean
+var user {
+  name: 'asdas'
+} //object
+
+var go = function() {
+  console.log('sadd')
+} //function
